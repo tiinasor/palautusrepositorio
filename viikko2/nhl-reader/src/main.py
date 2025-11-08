685e0f1539d6412cbb5ca2401dc1a0891f61a0d1
Tiina Vilkko <vilkkotiina@gmail.com>
2025-11-08 18:05:07 +0200

    3. Siistimpi pelaajalista

--- viikko2/nhl-reader/src/main.py
+++ viikko2/nhl-reader/src/main.py
@@ -7,10 +7,16 @@ def main():
 
     players = [Player(d) for d in response]
 
-    print("Players from FIN:\n")
-    for p in players:
-        if p.nationality == 'FIN':
-            print(p)
+
+    fin_players = list(filter(lambda pl: pl.nationality == 'FIN', players))
+
+    fin_sorted = sorted(fin_players, key=lambda pl: pl.goals + pl.assists, reverse=True)
+
+    print("Players from FIN\n")
+
+    for p in fin_sorted:
+        points = p.goals + p.assists
+        print(f"{p.name:<20} {p.team:<15} {p.goals:>2} + {p.assists:>2} = {points}")
 
 if __name__ == "__main__":
     main()
