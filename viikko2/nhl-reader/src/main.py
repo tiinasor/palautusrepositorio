91c6399ba2210e5ef153108fe073e114af97de67
Tiina Vilkko <vilkkotiina@gmail.com>
2025-11-09 16:27:14 +0200

    5. Graafinen pelaajalista

--- viikko2/nhl-reader/src/main.py
+++ viikko2/nhl-reader/src/main.py
@@ -1,14 +1,30 @@
 from player_reader import PlayerReader, PlayerStats
+import argparse
+from rich.prompt import Prompt
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    reader = PlayerReader(url)
-    stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality("FIN")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--season', type=str, default=None)
+    parser.add_argument('--nationality', type=str, default=None)
 
-    print("Players from FIN\n")
-    for player in players:
-        print(player)
+    args = parser.parse_args()
+
+    seasons = ["2018-19", "2019-20", "2020-21", "2021-22", "2022-23", "2023-24", "2024-25", "2025-26"]
+    nationalities = ["USA", "FIN", "CAN", "SWE", "CZE", "RUS", "SLO", "FRA", "GBR", "SVK", "DEN", "NED", "AUT", "BLR", "GER", "SUI", "NOR", "UZB", "LAT", "AUS"]
+
+    season = args.season or Prompt.ask("Season", choices=seasons, default="2024-25")
+    reader = PlayerReader(season)
+    stats = PlayerStats(reader)
+
+    first = True
+    while True:
+        if first and args.nationality:
+            nat = args.nationality
+        else:
+            nat = Prompt.ask("Nationality", choices=nationalities, default="")
+        first = False
+
+        stats.print_stats(nat)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player_reader.py
+++ viikko2/nhl-reader/src/player_reader.py
@@ -1,9 +1,14 @@
 import requests
 from player import Player
+from rich.console import Console
+from rich.table import Table
+
+BASE_URL = "https://studies.cs.helsinki.fi/nhlstats"
 
 class PlayerReader:
-    def __init__(self, url: str):
-        self.url = url
+    def __init__(self, season: str):
+        self.season = season
+        self.url = f"{BASE_URL}/{season}/players"
 
     def get_players(self):
         response = requests.get(self.url).json()
@@ -17,3 +22,29 @@ class PlayerStats:
         players = self.reader.get_players()
         selected = list(filter(lambda p: p.nationality == nationality, players))
         return sorted(selected, key=lambda p: p.goals + p.assists, reverse=True)
+
+    def print_stats(self, nationality: str):
+        console = Console()
+
+        table = Table(show_header=True, padding=(0, 2))
+        table.add_column("Released", style="cyan")
+        table.add_column("teams", style="magenta")
+        table.add_column("goals", justify="right", style="green")
+        table.add_column("assists", justify="right", style="green")
+        table.add_column("points", justify="right", style="green")
+
+        for player in self.top_scorers_by_nationality(nationality):
+            table.add_row(
+                player.name,
+                player.team,
+                str(player.goals),
+                str(player.assists),
+                str(player.goals + player.assists),
+            )
+
+        header = Table(show_header=False, width=len(table.columns) * 12, padding=(0,0), show_edge=False, show_lines=False)
+        header.add_column("header", justify="center")
+        header.add_row(f"[italic]Season {self.reader.season} players from {nationality}[/italic]")
+
+        console.print(header)
+        console.print(table)
