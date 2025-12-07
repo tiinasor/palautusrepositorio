c7c0b65f5fb01fd198ab73f8b3fd89137e464eb8
Tiina Vilkko <vilkkotiina@gmail.com>
2025-12-07 18:14:25 +0200

    4. Kyselykieli NHL-tilastoihin, osa 2

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,14 +1,23 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    filtered_with_all = stats.matches(All())
-    print(len(filtered_with_all))
+    matcher = And(
+        HasAtLeast(70, "points"),
+        Or(
+            PlaysIn("COL"),
+            PlaysIn("FLA"),
+            PlaysIn("BOS")
+        )
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
 
 
 if __name__ == "__main__":
